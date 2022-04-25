656fa19e18260436427610910cdef10c0fa6fd71
Eric Larson <larson.eric.d@gmail.com>
2022-04-25 16:18:33 -0400

    FIX: Pick

--- eeglabio/tests/test_raw.py
+++ eeglabio/tests/test_raw.py
@@ -1,7 +1,6 @@
 from os import path as op
 from pathlib import Path
 
-import pytest
 import numpy as np
 from mne.io import read_raw_fif, read_raw_eeglab
 from numpy.testing import assert_allclose
@@ -13,12 +12,11 @@ raw_fname = Path(__file__).parent / "data" / "test_raw.fif"
 
 def test_export_set(tmpdir):
     """Test saving a Raw instance to EEGLAB's set format"""
-    raw = read_raw_fif(raw_fname)
-    raw.load_data()
+    raw = read_raw_fif(raw_fname).pick_types(
+        meg=True, eeg=True, ecg=True).load_data()
     temp_fname = op.join(str(tmpdir), 'test_raw.set')
     export_mne_raw(raw, temp_fname)
-    with pytest.warns(RuntimeWarning, match='Not setting positions'):
-        raw_read = read_raw_eeglab(temp_fname, preload=True)
+    raw_read = read_raw_eeglab(temp_fname, preload=True)
     assert raw.ch_names == raw_read.ch_names
     cart_coords = np.array([d['loc'][:3] for d in raw.info['chs']])  # just xyz
     cart_coords_read = np.array([d['loc'][:3] for d in raw_read.info['chs']])
